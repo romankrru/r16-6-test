5f5c5e35d3d079ed1cfeeefdf42af9e45caf82d8
romankrru <rm07ru@gmail.com>
2018-11-04 17:56:48 +0400

    React.lazy

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import React, { Component } from "react";
-import Login from "./Components/Login";
-import Profile from "./Components/Profile";
+import React, { Component, Suspense } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import User from "./Components/User";
+// import Posts from "./Components/Posts";
+
+const Posts = React.lazy(() => import("./Components/Posts"));
+
+console.log(Suspense);
 
 class App extends Component {
-  state = {
-    isAuth: false
-  };
-
-  login = () =>
-    this.setState({
-      isAuth: true
-    });
-
-  logout = () =>
-    this.setState({
-      isAuth: false
-    });
-
   render() {
     return (
-      <React.Fragment>
-        <Login
-          isAuth={this.state.isAuth}
-          login={this.login}
-          logout={this.logout}
-        />
-        <Profile isAuth={this.state.isAuth} />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <nav>
+            <NavLink to="/user">User page</NavLink>
+            <NavLink to="/posts">Posts page</NavLink>
+          </nav>
+          <Route component={Welcome} path="/" exact />
+          <Route component={User} path="/user" />
+          <Route
+            render={() => (
+              <Suspense fallback={<div>Loading....</div>}>
+                <Posts />
+              </Suspense>
+            )}
+            path="/posts"
+          />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Components/Posts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Posts = () => {
+  return <h2>Posts page!</h2>;
+};
+
+export default Posts;
